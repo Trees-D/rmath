ff7b0f54908a426def93136167df2d7724d70b61
Trees-D <paladin100100@gmail.com>
2021-09-30 17:28:42 +0800

    add Transform

--- src/algebra/mod.rs
+++ src/algebra/mod.rs
@@ -3,3 +3,6 @@ pub use vec::{vec2d, vec2f, vec3d, vec3f, vec4d, vec4f, Vec2d, Vec2f, Vec3d, Vec
 
 mod mat;
 pub use mat::{mat2d, mat2f, mat3d, mat3f, mat4d, mat4f, Mat2d, Mat2f, Mat3d, Mat3f, Mat4d, Mat4f};
+
+mod transform;
+pub use transform::{Transform, Transformable};
---
+++ src/algebra/transform.rs
@@ -0,0 +1,159 @@
+use std::{fmt::Display, ops::Mul};
+
+use super::{Mat4d, Mat4f, Vec3d, Vec3f, Vec4d};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Transform {
+    matrix: Mat4d,
+}
+
+pub trait Transformable {
+    fn apply(&self, transform: Transform) -> Self;
+    fn apply_matrix4d(&self, matrix: Mat4d) -> Self;
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self {
+            matrix: Mat4d::identity(),
+        }
+    }
+}
+
+impl Display for Transform {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let m = self.matrix.to_array();
+        write!(
+            f,
+            "Transform(matrix: ({} {} {} {} | {} {} {} {} | {} {} {} {} | {} {} {} {}))",
+            m[0],
+            m[1],
+            m[2],
+            m[3],
+            m[4],
+            m[5],
+            m[6],
+            m[7],
+            m[8],
+            m[9],
+            m[10],
+            m[11],
+            m[12],
+            m[13],
+            m[14],
+            m[15]
+        )
+    }
+}
+
+impl Transform {
+    pub fn from_matrix4d(matrix: Mat4d) -> Self {
+        Self { matrix }
+    }
+
+    pub fn from_matrix4f(matrix: Mat4f) -> Self {
+        Self {
+            matrix: matrix.to_mat4d(),
+        }
+    }
+
+    pub fn matrix(&self) -> Mat4d {
+        self.matrix
+    }
+
+    pub fn inverse(&self) -> Self {
+        Self {
+            matrix: self.matrix.inverse(),
+        }
+    }
+}
+
+impl Mul<Transform> for Transform {
+    type Output = Transform;
+
+    fn mul(self, rhs: Transform) -> Self::Output {
+        self.apply(rhs)
+    }
+}
+
+impl Transform {
+    pub fn transform_homogeneous_coord(&self, coord: Vec4d) -> Vec4d {
+        self.matrix * coord
+    }
+
+    pub fn transform_point3f(&self, point: Vec3f) -> Vec3f {
+        let p = point.to_homogeneous_coord_point().to_vec4d();
+        let p = self.transform_homogeneous_coord(p);
+        if p.w().abs() > f64::EPSILON {
+            (p.xyz() / p.w()).to_vec3f()
+        } else {
+            p.xyz().to_vec3f()
+        }
+    }
+
+    pub fn transform_vector3f(&self, vector: Vec3f) -> Vec3f {
+        let v = vector.to_homogeneous_coord_vector().to_vec4d();
+        let v = self.transform_homogeneous_coord(v);
+        if v.w().abs() > f64::EPSILON {
+            (v.xyz() / v.w()).to_vec3f()
+        } else {
+            v.xyz().to_vec3f()
+        }
+    }
+
+    pub fn transform_normal3f(&self, normal: Vec3f) -> Vec3f {
+        let m = self.matrix.inverse().transpose();
+        let n = normal.to_homogeneous_coord_vector().to_vec4d();
+        let n = m * n;
+        if n.w().abs() > f64::EPSILON {
+            (n.xyz() / n.w()).to_vec3f()
+        } else {
+            n.xyz().to_vec3f()
+        }
+    }
+
+    pub fn transform_point3d(&self, point: Vec3d) -> Vec3d {
+        let p = point.to_homogeneous_coord_point();
+        let p = self.transform_homogeneous_coord(p);
+        if p.w().abs() > f64::EPSILON {
+            p.xyz() / p.w()
+        } else {
+            p.xyz()
+        }
+    }
+
+    pub fn transform_vector3d(&self, vector: Vec3d) -> Vec3d {
+        let v = vector.to_homogeneous_coord_vector();
+        let v = self.transform_homogeneous_coord(v);
+        if v.w().abs() > f64::EPSILON {
+            v.xyz() / v.w()
+        } else {
+            v.xyz()
+        }
+    }
+
+    pub fn transform_normal3d(&self, normal: Vec3d) -> Vec3d {
+        let m = self.matrix.inverse().transpose();
+        let n = normal.to_homogeneous_coord_vector();
+        let n = m * n;
+        if n.w().abs() > f64::EPSILON {
+            n.xyz() / n.w()
+        } else {
+            n.xyz()
+        }
+    }
+
+    pub fn transform<T: Transformable>(&self, object: T) -> T {
+        object.apply(*self)
+    }
+}
+
+impl Transformable for Transform {
+    fn apply(&self, transform: Transform) -> Self {
+        self.apply_matrix4d(transform.matrix)
+    }
+
+    fn apply_matrix4d(&self, matrix: Mat4d) -> Self {
+        Self::from_matrix4d(matrix * self.matrix)
+    }
+}
